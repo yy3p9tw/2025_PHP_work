8975a98e6a9dc6bcba3600fcd0c210fa444e4e26
yy3p9tw <yy3p9tw@gmail.com>
2025-05-16 15:51:30 +0800

    新增簡易登入

---
+++ 20250516/check.php
@@ -0,0 +1,10 @@
+<?php
+
+if($_POST['acc'] == 'admin' && $_POST['pw'] == '1234'){
+    echo "登入成功";    
+}else{
+    echo "登入失敗";
+}   
+
+
+?>
---
+++ 20250516/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員登入</title>
+</head>
+<body>
+    <form action="check.php" method='post'>
+        <div>
+            <label for="acc">帳號:</label>
+            <input type="text" name="acc" step="0.01" min="0" required>
+        </div>
+        <div>
+            <label for="pw">密碼:</label>
+            <input type="text" name="pw">
+        </div>
+
+        <input type="submit" value="登入">
+        <input type="reset" value="清空內容">
+    </form>
+
+    
+</body>
+</html>
+
