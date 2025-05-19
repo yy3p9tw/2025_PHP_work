2dabd2e63542469549e246032427d13f8e8c5882
yy3p9tw <yy3p9tw@gmail.com>
2025-05-19 10:23:01 +0800

    修改seession備註

--- 20250519/seession/check.php
+++ 20250519/seession/check.php
@@ -5,10 +5,10 @@ session_start();
 if($_POST['acc'] == 'admin' && $_POST['pw'] == '1234'){
     echo "登入成功";    
     $_SESSION['login']=1;
-    //header("location:login.php");
+    header("location:login.php");
 }else{
     echo "登入失敗";
-    // header("location:login.php");
+    header("location:login.php");
 }   
 
 ?>
