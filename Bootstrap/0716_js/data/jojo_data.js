ca6d496a7bb905729d90a3d4eb3a4db767256cc3
yy3p9tw <yy3p9tw@gmail.com>
2025-07-17 10:22:06 +0800

    上課內容

---
+++ Bootstrap/0716_js/data/jojo_data.js
@@ -0,0 +1,150 @@
+const data = [
+  // 第一部
+  {
+    id: 1,
+    name: "喬納森·喬斯達",
+    stand: "波紋",
+    description: "第一部主角，正直善良，波紋使者。",
+    image_url: "./img/01.jpg",
+    famous_quote: "我不會讓你得逞，DIO！"
+  },
+  {
+    id: 2,
+    name: "迪奧·布蘭度",
+    stand: "世界（The World）",
+    description: "第一部反派，野心勃勃，成為吸血鬼。",
+    image_url: "./img/02.jpg",
+    famous_quote: "你以為你贏了嗎？JOJO！"
+  },
+  // 第二部
+  {
+    id: 3,
+    name: "喬瑟夫·喬斯達",
+    stand: "波紋/隱者之紫（Hermit Purple）",
+    description: "第二部主角，機智多謀，波紋與替身雙修。",
+    image_url: "./img/03.jpg",
+    famous_quote: "你的下一句話是……！"
+  },
+  {
+    id: 4,
+    name: "西撒·安東尼奧·齊貝林",
+    stand: "波紋",
+    description: "第二部主角團，波紋使者，喬瑟夫摯友。",
+    image_url: "./img/04.jpg",
+    famous_quote: "齊貝林家族的榮耀！"
+  },
+  {
+    id: 5,
+    name: "莉莎莉莎",
+    stand: "波紋",
+    description: "第二部主角團，波紋大師，喬瑟夫母親。",
+    image_url: "./img/05.jpg",
+    famous_quote: "波紋疾走！"
+  },
+  {
+    id: 6,
+    name: "卡茲",
+    stand: "究極生物",
+    description: "第二部最終反派，柱之男首領，追求究極生命體。",
+    image_url: "./img/06.jpg",
+    famous_quote: "我就是究極生命體！"
+  },
+  // 第三部
+  {
+    id: 7,
+    name: "空條承太郎",
+    stand: "白金之星（Star Platinum）",
+    description: "第三部主角，冷靜沉著，擁有強大替身能力。",
+    image_url: "./img/07.jpg",
+    famous_quote: "無駄無駄無駄無駄！"
+  },
+  {
+    id: 8,
+    name: "喬瑟夫·喬斯達",
+    stand: "隱者之紫（Hermit Purple）",
+    description: "第三部主角團，承太郎祖父，波紋與替身雙修。",
+    image_url: "./img/08.jpg",
+    famous_quote: "歐拉歐拉歐拉！"
+  },
+  {
+    id: 9,
+    name: "花京院典明",
+    stand: "法皇之綠（Hierophant Green）",
+    description: "第三部主角團，替身可遠距離攻擊。",
+    image_url: "./img/09.jpg",
+    famous_quote: "レロレロ レロレロ レロレロ レロレロ レロレロ レロレロ レロレロ レロレロ レロレロ"
+  },
+  {
+    id: 10,
+    name: "穆罕默德·阿布德爾",
+    stand: "魔術師紅（Magician's Red）",
+    description: "第三部主角團，火焰替身使者。",
+    image_url: "./img/10.jpg",
+    famous_quote: "波魯那雷夫！這是命運！"
+  },
+  {
+    id: 11,
+    name: "波魯那雷夫",
+    stand: "銀色戰車（Silver Chariot）",
+    description: "第三部主角團，劍術高手。",
+    image_url: "./img/11.jpg",
+    famous_quote: "我叫波魯那雷夫，是個法國人！"
+  },
+  {
+    id: 12,
+    name: "伊奇",
+    stand: "愚者（The Fool）",
+    description: "第三部主角團，狗狗替身使者。",
+    image_url: "./img/12.jpg",
+    famous_quote: "汪！"
+  },
+  {
+    id: 13,
+    name: "DIO",
+    stand: "世界（The World）",
+    description: "第三部最終反派，擁有時間暫停能力。",
+    image_url: "./img/13.jpg",
+    famous_quote: "世界！時間停止！"
+  },
+  // 第四部
+  {
+    id: 14,
+    name: "東方仗助",
+    stand: "瘋狂鑽石（Crazy Diamond）",
+    description: "第四部主角，溫柔正義，治癒系替身。",
+    image_url: "./img/14.jpg",
+    famous_quote: "你罵我頭髮？"
+  },
+  {
+    id: 15,
+    name: "廣瀨康一",
+    stand: "回音（Echoes）",
+    description: "第四部主角團，成長型替身使者。",
+    image_url: "./img/15.jpg",
+    famous_quote: "康一君要加油！"
+  },
+  {
+    id: 16,
+    name: "虹村億泰",
+    stand: "壞公司（The Hand）",
+    description: "第四部主角團，單純善良，空間抹除能力。",
+    image_url: "./img/16.jpg",
+    famous_quote: "這傢伙不簡單！"
+  },
+  {
+    id: 17,
+    name: "岸邊露伴",
+    stand: "天堂之門（Heaven's Door）",
+    description: "第四部主角團，漫畫家，能閱讀與改寫他人記憶。",
+    image_url: "./img/17.jpg",
+    famous_quote: "我可是岸邊露伴，不會說謊！"
+  },
+  {
+    id: 18,
+    name: "吉良吉影",
+    stand: "殺手皇后（Killer Queen）",
+    description: "第四部最終反派，隱藏於杜王町的連環殺手。",
+    image_url: "./img/18.jpg",
+    famous_quote: "我要平靜地生活。"
+  }
+];
